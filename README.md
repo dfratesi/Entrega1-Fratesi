8eb11ee446a6278713f25e3c2499eb970d116c30
Damian Fratesi <dfratesi@users.noreply.github.com>
2022-08-05 15:50:08 -0300

    agregar campos

---
+++ README.md
@@ -0,0 +1,1 @@
+# Preentrega Trabajo Final
--- libreria/models.py
+++ libreria/models.py
@@ -7,9 +7,19 @@ class Libro(models.Model):
     title = models.CharField("Título", max_length=50)
     editorial = models.CharField("Editorial", max_length=50, null=True, blank=True)
     isbn = models.CharField("ISBN", max_length=13, null=True, blank=True)
+    price = models.FloatField("Precio")
     idioma = models.ForeignKey(
         "Idioma", on_delete=models.SET_NULL, null=True, blank=True
     )
+    genero = models.ManyToManyField("Genero", verbose_name="Género")
+    autor = models.ForeignKey(
+        "Autor",
+        verbose_name="Autor",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+    )
+    resumen = models.TextField("Resumen", max_length=1000, null=True, blank=True)
 
     def __str__(self):
         return self.title
@@ -21,6 +31,7 @@ class Libro(models.Model):
 class Autor(models.Model):
     name = models.CharField("Nombre", max_length=20, null=True, blank=True)
     last_name = models.CharField("Apellido", max_length=50)
+    nacionalidad = models.CharField("Nacionalidad", max_length=50, null=True, blank=True)
 
     class Meta:
         ordering = ["last_name", "name"]
@@ -36,6 +47,10 @@ class Genero(models.Model):
         max_length=100,
         help_text="Ingresa un género de libro (ej. Ciencia Ficción, Misterio)",
     )
+
+    def __str__(self):
+        return self.name
+    
 
 
 class Idioma(models.Model):
