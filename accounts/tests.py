9e32c8723b619b8ac157cccc61717cf8a83d0f85
Damian Fratesi <dfratesi@users.noreply.github.com>
2022-08-16 23:10:34 -0300

    modificacion de test user

--- accounts/tests.py
+++ accounts/tests.py
@@ -6,10 +6,10 @@ class CustomUserTests(TestCase):
     def test_create_user(self):
         User = get_user_model()
         user = User.objects.create_user(
-            username="john", email="test@email.com", password="testpass123"
+            username="damian", email="damian@gmail.com", password="password123"
         )
-        self.assertEqual(user.username, "john")
-        self.assertEqual(user.email, "test@email.com")
+        self.assertEqual(user.username, "damian")
+        self.assertEqual(user.email, "damian@gmail.com")
         self.assertTrue(user.is_active)
         self.assertFalse(user.is_staff)
         self.assertFalse(user.is_superuser)
@@ -17,10 +17,10 @@ class CustomUserTests(TestCase):
     def test_create_superuser(self):
         User = get_user_model()
         user = User.objects.create_superuser(
-            username="superadmin", email="superadmin@email.com", password="testpass123"
+            username="admin", email="superadmin@gmail.com", password="password123"
         )
-        self.assertEqual(user.username, "superadmin")
-        self.assertEqual(user.email, "superadmin@email.com")
+        self.assertEqual(user.username, "admin")
+        self.assertEqual(user.email, "superadmin@gmail.com")
         self.assertTrue(user.is_active)
         self.assertTrue(user.is_staff)
         self.assertTrue(user.is_superuser)
